7189f66a038d8c1c29d656d6a1448ed7223aca88
Ivan Cisneros <ivanalexiscisneros@college.harvard.edu>
2016-11-22 22:05:55 -0500

    better, but not quite accurate.  still have that max distance issue

--- Development/rangefinder.py
+++ Development/rangefinder.py
@@ -25,23 +25,32 @@ class RangeFinder:
 
 
 	def distance_read(self):
+		# send ultrasonic pulse
 		GPIO.output(self.TRIG, True)
-		time.sleep(0.00001)
+		time.sleep(0.00001)  # 10uS pules
 		GPIO.output(self.TRIG, False)
 
+		# listen for the echo
 		while GPIO.input(self.ECHO)==0:
-		  pulse_start = time.time()
+			pulse_start = time.time()
 
+		# echo was heard
 		while GPIO.input(self.ECHO)==1:
-		  pulse_end = time.time()
+			pulse_end = time.time()
 
+		# time differential; time.time() returns the time in seconds
 		pulse_duration = pulse_end - pulse_start
 
-		distance = pulse_duration * 17150
+		distance = pulse_duration * 17150  # 17150 = (34300 cm/s) / 2
+		distance = round(distance, 2)  # distance is in cm
 
-		distance = round(distance, 2)
+		distance_m = pulse_duration * 171.50  # 171.50 = (343 m/s) / 2
+		distance_m = round(distance, 2)  # distance is in meters
 
-		print "Distance:",distance,"cm"
+		print "Raw travel time: ",pulse_duration,"s"
+		print "Distance (meters):",distance_m,"meters"
+
+		print "Distance (cm):",distance,"cm\n"
 
 
 	def finish(self):
