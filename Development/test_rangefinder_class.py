4c04514319c0c1a29d79fc1a90d405d6a19b1541
Ivan Cisneros <ivanalexiscisneros@college.harvard.edu>
2016-11-22 23:51:57 -0500

    return the median instead

--- Development/test_rangefinder_class.py
+++ Development/test_rangefinder_class.py
@@ -1,20 +1,30 @@
 from rangefinder import RangeFinder
+import numpy as np
 
 
-def mean_distance():
+def median_distance():
+	"""Takes 3 readings. If 2/3 values are non zero, return the median value. Else
+	   returns 0.0
+	"""
+
 	distances = []
 
 	for i in range(0,3):	
 		distances.append(rf.distance_read())
 		rf.wait()
 
-	suff_nonzero = 0
+	num_nonzero = 0
 	for i in range(0,3):
-		if distances[1] != 0:
-			suff_nonzero += 1
+		if distances[1] != 0.0:
+			num_nonzero += 1
+	
+	# remove the zeros
+	while 0.0 in distances:
+		distances.remove(0.0)
 
-	if suff_nonzero >= 2:
-		return reduce(lambda x, y: x + y, distances) / suff_nonzero
+	if num_nonzero >= 2:  # if 2/3 values are non zero, return the median
+		return np.median(distances)
+		# return reduce(lambda x, y: x + y, distances) / num_nonzero
 	else:
 		return 0.0
 
@@ -33,7 +43,7 @@ if __name__ == '__main__':
 	try:
 		# For continuous reading
 		while True:
-			print "Mean Distance: ", mean_distance(),"cm\n"
+			print "Median Distance: ", median_distance(),"cm\n"
 
 		# For a single shot read
 		# rf.distance_read()
