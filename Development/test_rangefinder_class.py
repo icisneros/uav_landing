16897e2f231567b62ee84d98b921bf95b84f7ea1
Ivan Cisneros <ivanalexiscisneros@college.harvard.edu>
2016-11-22 23:35:21 -0500

    mean distance finder, reordered

--- Development/test_rangefinder_class.py
+++ Development/test_rangefinder_class.py
@@ -1,6 +1,25 @@
 from rangefinder import RangeFinder
 
 
+def mean_distance():
+	distances = []
+
+	for i in range(0,5):	
+		distances.append(rf.distance_read())
+		rf.wait()
+
+	suff_nonzero = 0
+	for i in range(0,5):
+		if distances[1] != 0:
+			suff_nonzero += 1
+
+	if suff_nonzero >= 3:
+		return 0.0
+	else:
+		return reduce(lambda x, y: x + y, distances) / len(distances)
+
+
+
 if __name__ == '__main__':
 	rf = RangeFinder()
 
@@ -20,22 +39,3 @@ if __name__ == '__main__':
 		# rf.distance_read()
 	except KeyboardInterrupt:
 		rf.finish()
-
-
-
-def mean_distance():
-	distances = []
-
-	for i in range(0,5):	
-		distances.append(rf.distance_read())
-		rf.wait()
-
-	suff_nonzero = 0
-	for i in range(0,5):
-		if distances[1] != 0:
-			suff_nonzero += 1
-
-	if suff_nonzero >= 3:
-		return 0.0
-	else:
-		return reduce(lambda x, y: x + y, distances) / len(distances)
